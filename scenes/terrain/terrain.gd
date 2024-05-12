fb65aa18acb44a68e47d22cebdaca8bab3c131f2
Flores <pascalthomas.flores@gmail.com>
2024-05-12 02:53:48 +0200

    modified generation methods for better performance

--- scenes/terrain/terrain.gd
+++ scenes/terrain/terrain.gd
@@ -42,12 +42,12 @@ func snap_to_grid(position : Vector2i) -> Vector2i:
 	return Vector2(position.x - posmod(position.x, cell_size), position.y - posmod(position.y, cell_size));
 	
 func next_cell_generation():
-	var possible_new_cells_positions : Array[Vector2] = [];
+	var possible_new_cells_positions : Dictionary = {};
 	var new_cells : Array = [];
 	var dead_cells : Array = [];
 	for cell in get_children():
 		add_possible_new_cells(cell, possible_new_cells_positions);
-		if not (cell.get_neighbours_number() == 2 or cell.get_neighbours_number() == 3):
+		if not (cell.get_adjacent_cells_number() == 2 or cell.get_adjacent_cells_number() == 3):
 			dead_cells.append(cell);
 	new_cells = create_new_cells(possible_new_cells_positions);
 	for new_cell in new_cells:
@@ -55,29 +55,27 @@ func next_cell_generation():
 	for dead_cell in dead_cells:
 		dead_cell.queue_free();
 
-func add_possible_new_cells(cell : Node2D, possible_new_cells_positions : Array[Vector2]):
+func add_possible_new_cells(cell: Node2D, possible_new_cells_positions : Dictionary):
 	var offsets : Array[Vector2] = [
 		Vector2(-cell_size, -cell_size),	Vector2(0, -cell_size),		Vector2(cell_size, -cell_size),
 		Vector2(-cell_size, 0),											Vector2(cell_size, 0),
 		Vector2(-cell_size, cell_size), 	Vector2(0, cell_size),		Vector2(cell_size, cell_size)
 	]
-	var cell_neighbours : Array[Area2D] = cell.get_neighbours();
+	var cell_neighbours : Array[Area2D] = cell.get_adjacent_cells();
 	var offset_position : Vector2;
 	for offset in offsets:
 		offset_position = cell.position + offset;
 		if not cell_neighbours.any(func(neighbour : Area2D): return (neighbour.global_position == offset_position)):
-			possible_new_cells_positions.append(offset_position);
-
-func create_new_cells(possible_new_cells_positions : Array[Vector2]) -> Array:
-	var new_cells : Array[Node2D] = [];
-	var new_cells_positions = [];
-	for maybe_duplicate_new_cell_position in possible_new_cells_positions.filter(func(position : Vector2): return possible_new_cells_positions.count(position) == 3):
-			if not new_cells_positions.has(maybe_duplicate_new_cell_position):
-				new_cells_positions.append(maybe_duplicate_new_cell_position);
+			if not possible_new_cells_positions.has(offset_position):
+				possible_new_cells_positions[offset_position] = 1;
+			else:
+				possible_new_cells_positions[offset_position] += 1;
 	
-	for new_cell_position in new_cells_positions:
-		var new_cell = cell_scene.instantiate();
-		new_cell.position = new_cell_position;
-		new_cells.append(new_cell);
-		
+func create_new_cells(possible_new_cells_positions : Dictionary):
+	var new_cells : Array = [];
+	for possible_new_cell_position in possible_new_cells_positions:
+		if possible_new_cells_positions[possible_new_cell_position] == 3:
+			var new_cell = cell_scene.instantiate();
+			new_cell.position = possible_new_cell_position;
+			new_cells.append(new_cell);
 	return new_cells;
